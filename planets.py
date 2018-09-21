e08f18464d28cc3fbe680baff24f00866d63842f
Noah Gray <njgray@calpoly.edu>
2018-09-21 12:54:33 -0700

    Conversions Added

--- planets.py
+++ planets.py
@@ -1,7 +1,7 @@
 def weight_on_planets():
-   # write your code here
-   
-   
+
+    lb = int(input("What do you weigh on earth? "))
+    print("\nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds.".format(lb * .38, lb * 2.34))
    
 if __name__ == '__main__':
    weight_on_planets()
